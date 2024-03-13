e4c24727facecd2962c28dd019616be2bf036133
Veager-k <79209135+Veager-k@users.noreply.github.com>
2024-03-13 16:50:49 +0200

    test with data2.txt

--- src/test/java/TestFour/TestFour.java
+++ src/test/java/TestFour/TestFour.java
@@ -151,4 +151,79 @@ public class TestFour {
         
         assertTrue(result.contentEquals("Your order has been successfully processed!"));
 	}
+	
+	@Test
+	@Order(3)
+	public void test2() throws InterruptedException {
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/login']"))).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Email"))).sendKeys(email);
+		
+		driver.findElement(By.id("Password")).sendKeys("password1");
+		
+		driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/digital-downloads']"))).click();
+		
+
+		
+		try {
+		      File myObj = new File("C:\\Users\\viliu\\eclipse-workspace\\TestOne\\src\\main\\java\\firstTest\\data2.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine().toLowerCase().replaceAll(" ", "-");        
+		        
+		        if(data.contentEquals("3rd-album")) {
+		        	data = "album-3";
+		        }
+		        
+		        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/" + data +"']"))).click();
+		        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='button-1 add-to-cart-button']"))).click();
+		        
+		        Thread.sleep(1000);
+		        driver.navigate().back();
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='cart-label']"))).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("termsofservice"))).click();
+		
+		driver.findElement(By.xpath("//button[@class='button-1 checkout-button']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("BillingNewAddress.City"))).sendKeys("ASDF");
+		
+		driver.findElement(By.name("BillingNewAddress.Address1")).sendKeys("ASDF");
+		
+		driver.findElement(By.name("BillingNewAddress.ZipPostalCode")).sendKeys("1234");
+		
+		driver.findElement(By.name("BillingNewAddress.PhoneNumber")).sendKeys("1234");
+		
+		WebElement selectElement = driver.findElement(By.id("BillingNewAddress_CountryId"));
+        Select select = new Select(selectElement);
+        select.selectByValue("1");
+
+        driver.findElement(By.xpath("//input[@class='button-1 new-address-next-step-button']")).click();
+		
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='button-1 payment-method-next-step-button']"))).click();
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='button-1 payment-info-next-step-button']"))).click();
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='button-1 confirm-order-next-step-button']"))).click();
+        
+        
+        
+        WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='title']")));
+        String result = title.getText();
+        
+        
+        assertTrue(result.contentEquals("Your order has been successfully processed!"));
+	}
 }
